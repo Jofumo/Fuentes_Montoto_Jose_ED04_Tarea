a8c002c32800fbbad57022e48e74a762ea1aa83c
Jose Fuentes Montoto <a20josefm@iessanclemente.net>
2022-03-13 19:10:38 +0100

    Código fuente actualizado

--- Fuentes_Montoto_Jose_ED04_Tarea/src/main/java/cuentas/CCuenta.java
+++ Fuentes_Montoto_Jose_ED04_Tarea/src/main/java/cuentas/CCuenta.java
@@ -9,6 +9,62 @@ package cuentas;
  * @author Jorge
  */
 public class CCuenta {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInteres
+     */
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+
+    /**
+     * @param tipoInteres the tipoInteres to set
+     */
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
+    }
     private String nombre;
     private String cuenta;
     private double saldo;
@@ -27,14 +83,14 @@ public class CCuenta {
 
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception
@@ -43,6 +99,6 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
 }
--- Fuentes_Montoto_Jose_ED04_Tarea/src/main/java/cuentas/Main.java
+++ Fuentes_Montoto_Jose_ED04_Tarea/src/main/java/cuentas/Main.java
@@ -7,23 +7,36 @@ package cuentas;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        
+        
+        CCuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        cuenta1 = operativa_cuenta();
 
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
     }
+
+    static CCuenta operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
+        double saldoActual;
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        return cuenta1;
+    }
+        
 }
+
+
+
